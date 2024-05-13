bd59fee5be4a6908c46bb7300ee809dfcb6a0599
Pranjali Yadav <51490690+Pranjaliyadav@users.noreply.github.com>
2024-05-14 01:21:03 +0530

    creating data provider and liveprovider of for refine and handing error and data fetching correctly using graphQL

--- lazy-socks-kneel/src/App.tsx
+++ lazy-socks-kneel/src/App.tsx
@@ -5,9 +5,8 @@ import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
-import dataProvider, {
+import  {
   GraphQLClient,
-  liveProvider,
 } from "@refinedev/nestjs-query";
 import routerBindings, {
   DocumentTitleHandler,
@@ -16,29 +15,31 @@ import routerBindings, {
 import { App as AntdApp } from "antd";
 import { createClient } from "graphql-ws";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { authProvider } from "./authProvider";
-import { ColorModeContextProvider } from "./contexts/color-mode";
+import { dataProvider, liveProvider } from "./providers";
 
+//URLs for GraphQL API and Websocket connection.  points to sever where GraphQL API is hosted, used for fetching and manipulating data
 const API_URL = "https://api.nestjs-query.refine.dev/graphql";
 const WS_URL = "wss://api.nestjs-query.refine.dev/graphql";
 
-const gqlClient = new GraphQLClient(API_URL);
-const wsClient = createClient({ url: WS_URL });
+// const gqlClient = new GraphQLClient(API_URL);
+// const wsClient = createClient({ url: WS_URL });
 
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
-      <RefineKbarProvider>
-        <ColorModeContextProvider>
+    {/* Displaying Github banner for displaying branding or lining to a repo */}
+      <GitHubBanner /> 
+      {/* for usage of Antd components */}
           <AntdApp>
+            {/* to provide development tools and feature */}
             <DevtoolsProvider>
+              {/* central hub for managing state, data fetching, application configuration */}
               <Refine
-                dataProvider={dataProvider(gqlClient)}
-                liveProvider={liveProvider(wsClient)}
+                dataProvider={dataProvider}
+                liveProvider={liveProvider}
                 notificationProvider={useNotificationProvider}
                 routerProvider={routerBindings}
-                authProvider={authProvider}
+                // authProvider={authProvider}
                 options={{
                   syncWithLocation: true,
                   warnWhenUnsavedChanges: true,
@@ -47,18 +48,20 @@ function App() {
                   liveMode: "auto",
                 }}
               >
+                {/* For definign app routes */}
                 <Routes>
                   <Route index element={<WelcomePage />} />
                 </Routes>
-                <RefineKbar />
+                {/* to provide keyboard driver interface for navigation and other actions  */}
+                {/* <RefineKbar /> */}
+                {/* features related to unsaved changes notification and document title management */}
                 <UnsavedChangesNotifier />
                 <DocumentTitleHandler />
               </Refine>
+              {/* development tools panel for debugging and monitoring */}
               <DevtoolsPanel />
             </DevtoolsProvider>
           </AntdApp>
-        </ColorModeContextProvider>
-      </RefineKbarProvider>
     </BrowserRouter>
   );
 }
