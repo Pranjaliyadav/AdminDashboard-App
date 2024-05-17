e4efe6191cf46c5636e570af8b575d456862fc71
Pranjali Yadav <51490690+Pranjaliyadav@users.noreply.github.com>
2024-05-17 23:41:40 +0530

    latest activities card data complete with home page

--- lazy-socks-kneel/src/components/home/latest-activities.tsx
+++ lazy-socks-kneel/src/components/home/latest-activities.tsx
@@ -1,22 +1,26 @@
 import { UnorderedListOutlined } from '@ant-design/icons'
-import { Card, List } from 'antd'
+import { Card, List, Space } from 'antd'
 import React from 'react'
 import { Text } from '../text'
 import LatestActivitiesSkeleton from '../skeleton/latest-activities'
 import { useList } from '@refinedev/core'
 import { DASHBOARD_LATEST_ACTIVITIES_AUDITS_QUERY, DASHBOARD_LATEST_ACTIVITIES_DEALS_QUERY } from '@/graphql/queries'
+import dayjs from 'dayjs'
+import CustomAvatar from '../custom-avatar'
+import { stringify } from 'querystring'
 
 const LatestActivities = () => {
 
-    const isLoading = false
+    //get all audits
     const { data: audit, isLoading: isLoadingAudit, isError, error } = useList({
         resource: 'audits',
         meta: {
             gqlQuery: DASHBOARD_LATEST_ACTIVITIES_AUDITS_QUERY
         }
     })
-
     const dealIds = audit?.data?.map((audit: any) => audit?.targetId)
+
+    //get all deals
     const { data: deals, isLoading: isLoadingDeals } = useList({
         resource: 'deals',
         queryOptions: { enabled: !!dealIds?.length },
@@ -28,7 +32,7 @@ const LatestActivities = () => {
             gqlQuery: DASHBOARD_LATEST_ACTIVITIES_DEALS_QUERY
         }
     })
-
+    const isLoading = isLoadingAudit || isLoadingDeals
     if (isError) {
         console.log(error)
         return error
@@ -66,7 +70,45 @@ const LatestActivities = () => {
                         )}
                     />)
                     :
-                    (<List />)
+                    (<List
+                        itemLayout="horizontal"
+                        dataSource={audit?.data}
+                        renderItem={(item) => {
+                            //use all audits and deals together
+                            const deal = deals?.data.find((deal => deal.id == item.targetId) || undefined)
+                            return (
+                                <List.Item>
+                                    <List.Item.Meta
+                                        title={dayjs(deal?.createdAt).format('MMM DD, YYYY - HH:mm')}
+                                        avatar={
+                                            <CustomAvatar
+                                                shape="square"
+                                                size={48}
+                                                src={deal?.company.avatarUrl}
+                                                name={deal?.company.name}
+                                            />
+                                        }
+                                        description={
+                                            <Space
+                                                size={4}
+                                            >
+
+                                                <Text strong >{item.user?.name}</Text>
+                                                <Text>{item.action === 'CREATE' ? 'created' : 'moved'}</Text>
+                                                <Text strong>{deal?.title}</Text>
+                                                <Text>deal</Text>
+                                                <Text>{item.action === 'CREATE' ? 'in' : 'to'}</Text>
+                                                <Text strong>{deal?.stage?.title}</Text>
+                                            </Space>
+                                        }
+
+                                    />
+
+                                </List.Item>
+                            )
+                        }}
+
+                    />)
             }
 
         </Card>
